9d5c40283e2d19f0df6314de3ea498b75fb68491
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2022-02-19 14:47:53 -0500

    set up refresh on image click

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 var queryUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 var quoteElement = document.querySelector("#quote");
-var pictureElement= document.querySelector('#picture')
-var ronSwansonPicutre = './Assets/ronSwansonPicture.jpeg'
-
-
+var pictureElement = document.querySelector("#picture");
+var ronSwansonElement = document.querySelector(".ronSwanson");
+var ronSwansonPicutre = "./Assets/ronSwansonPicture.jpeg";
 
 async function ronSwanson() {
   const response = await fetch(queryUrl);
   const data = await response.json();
   quoteElement.innerHTML = `<h1>"${data}"</h1>`;
-  pictureElement.innerHTML = `<img src="${ronSwansonPicutre}" alt="picture of Ron Swanson">`
+  pictureElement.innerHTML = `<a href="/"><img src="${ronSwansonPicutre}" class="ronSwanson" alt="picture of Ron Swanson"></a>`;
 }
 
-ronSwanson()
 
 
+ronSwanson();
